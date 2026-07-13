f2f81848a8018b38dc0a8d4d1c6e891dd379f9d0
Mohammedmushtaq0 <mohammedmushtaq0010@gmail.com>
2026-07-13 19:38:23 +0530

    added the ecr repositories of all main docker images

---
+++ infrastructure/terraform/ecrandtaskdefination.tf
@@ -0,0 +1,104 @@
+#################################
+# Main Application
+#################################
+
+resource "aws_ecr_repository" "main_app" {
+  name                 = "hourairos/main-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name = "hourairos-main-app"
+  }
+}
+
+#################################
+# OTEL Collector
+#################################
+
+resource "aws_ecr_repository" "otel_collector" {
+  name                 = "hourairos-otel-collector"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name = "hourairos-otel-collector"
+  }
+}
+
+#################################
+# Tempo
+#################################
+
+resource "aws_ecr_repository" "tempo" {
+  name                 = "hourairos-tempo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name = "hourairos-tempo"
+  }
+}
+
+#################################
+# OTEL Collector v2
+#################################
+
+resource "aws_ecr_repository" "otel_collector_v2" {
+  name                 = "hourairos-otel-collector-v2"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name = "hourairos-otel-collector-v2"
+  }
+}
+
+
+import {
+  to = aws_ecr_repository.main_app
+  id = "hourairos/main-app"
+}
+
+import {
+  to = aws_ecr_repository.otel_collector
+  id = "hourairos-otel-collector"
+}
+
+import {
+  to = aws_ecr_repository.tempo
+  id = "hourairos-tempo"
+}
+
+import {
+  to = aws_ecr_repository.otel_collector_v2
+  id = "hourairos-otel-collector-v2"
+}
