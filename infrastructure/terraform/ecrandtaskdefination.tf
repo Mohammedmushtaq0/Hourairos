eff60fdcbea1a11a6c406e0b380fbe7e486602fe
Mohammedmushtaq0 <mohammedmushtaq0010@gmail.com>
2026-07-14 16:56:38 +0530

    added the ecs cluster with it services

--- infrastructure/terraform/ecrandtaskdefination.tf
+++ infrastructure/terraform/ecrandtaskdefination.tf
@@ -102,3 +102,45 @@ import {
   to = aws_ecr_repository.otel_collector_v2
   id = "hourairos-otel-collector-v2"
 }
+
+resource "aws_ecs_task_definition" "demo_task" {
+  family = "demo_task"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "service-first"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    },
+    {
+      name      = "second"
+      image     = "service-second"
+      cpu       = 10
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  }
+}
--- infrastructure/terraform/ecs.tf
+++ infrastructure/terraform/ecs.tf
@@ -1,20 +1,67 @@
-# #################################
-# # ECS Cluster
-# #################################
+#################################
+# ECS Cluster
+#################################
 
-# resource "aws_ecs_cluster" "hourairos_cluster" {
-#   name = "hourairos-cluster"
+resource "aws_ecs_cluster" "hourairos_cluster" {
+  name = "hourairos-cluster"
 
-#   service_connect_defaults {
-#     namespace = aws_service_discovery_http_namespace.hourairos.arn
-#   }
+  service_connect_defaults {
+    namespace = aws_service_discovery_http_namespace.hourairos.arn
+  }
 
-#   tags = {
-#     Name = "hourairos-cluster"
-#   }
-# }
+  tags = {
+    Name = "hourairos-cluster"
+  }
+}
 
-# import {
-#   to = aws_ecs_cluster.hourairos_cluster
-#   id = "hourairos-cluster"
-# }
+import {
+  to = aws_ecs_cluster.hourairos_cluster
+  id = "hourairos-cluster"
+}
+
+#################################
+# ECS Services
+#################################
+
+resource "aws_ecs_service" "hourairos_app" {
+  name    = "hourairous-app-fargate-service-z4p2puiy"
+  cluster = aws_ecs_cluster.hourairos_cluster.id
+}
+
+import {
+  to = aws_ecs_service.hourairos_app
+  id = "hourairos-cluster/hourairous-app-fargate-service-z4p2puiy"
+}
+
+
+resource "aws_ecs_service" "grafana" {
+  name    = "hourairos-grafana-service-hsihj5bd"
+  cluster = aws_ecs_cluster.hourairos_cluster.id
+}
+
+import {
+  to = aws_ecs_service.grafana
+  id = "hourairos-cluster/hourairos-grafana-service-hsihj5bd"
+}
+
+
+resource "aws_ecs_service" "tempo" {
+  name    = "hourairos-tempo--service-c681eq7s"
+  cluster = aws_ecs_cluster.hourairos_cluster.id
+}
+
+import {
+  to = aws_ecs_service.tempo
+  id = "hourairos-cluster/hourairos-tempo--service-c681eq7s"
+}
+
+
+resource "aws_ecs_service" "otel_collector" {
+  name    = "hourairos-otel-collector-service-291tugr6"
+  cluster = aws_ecs_cluster.hourairos_cluster.id
+}
+
+import {
+  to = aws_ecs_service.otel_collector
+  id = "hourairos-cluster/hourairos-otel-collector-service-291tugr6"
+}
